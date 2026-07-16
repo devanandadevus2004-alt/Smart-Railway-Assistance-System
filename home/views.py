039807c089bb4ce7fe709bd3f2171dfa43b36c78
Devananda <devanandadevus2004@gmail.com>
2026-07-16 21:00:47 +0530

    Implement passenger login with Django messages

--- home/views.py
+++ home/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Passenger
 
 def home(request):
     return render(request, "home/index.html")
-
 def login(request):
+    print("Method:", request.method)
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            passenger = Passenger.objects.get(
+                username=username,
+                password=password
+            )
+            return render(request, "home/index.html", {
+                "name": passenger.full_name
+            })
+
+        except Passenger.DoesNotExist:
+            messages.error(request, "Invalid Username or Password")
+            return redirect("login")
+
+    print("GET request")
     return render(request, "home/login.html")
+
 def register(request):
     if request.method == "POST":
         full_name = request.POST["full_name"]
