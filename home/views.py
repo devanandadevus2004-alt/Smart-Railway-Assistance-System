bc46a19c149e385e034dd52b8b04c0c65e756db8
Devananda <devanandadevus2004@gmail.com>
2026-07-16 13:47:52 +0530

    Add login and registration pages

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "home/index.html")
+
+def login(request):
+    return render(request, "home/login.html")
+
+def register(request):
+    return render(request, "home/register.html")
