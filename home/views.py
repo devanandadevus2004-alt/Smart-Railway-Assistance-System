5c2bb39db7bf701776ebc65af9c6a59c8744e21c
Devananda <devanandadevus2004@gmail.com>
2026-07-16 19:40:14 +0530

    Implement passenger registration with MYSQL

--- home/views.py
+++ home/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from .models import Passenger
 
 def home(request):
     return render(request, "home/index.html")
 
 def login(request):
     return render(request, "home/login.html")
-
 def register(request):
+    if request.method == "POST":
+        full_name = request.POST["full_name"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        Passenger.objects.create(
+            full_name=full_name,
+            email=email,
+            username=username,
+            password=password
+        )
+
+        return render(request, "home/register.html", {
+            "success": "Registration Successful!"
+        })
+
     return render(request, "home/register.html")
