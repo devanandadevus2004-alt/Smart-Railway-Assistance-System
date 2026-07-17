dedca196c4d3aac01795479661416247f2b30cc5
Devananda <devanandadevus2004@gmail.com>
2026-07-17 19:51:43 +0530

    Improve passenger registration and authentication

--- home/models.py
+++ home/models.py
@@ -1,10 +1,39 @@
 from django.db import models
-
 class Passenger(models.Model):
     full_name = models.CharField(max_length=100)
+
     email = models.EmailField(unique=True)
+
     username = models.CharField(max_length=50, unique=True)
+
     password = models.CharField(max_length=100)
+
+    aadhaar_number = models.CharField(max_length=12, unique=True)
+
+    phone_number = models.CharField(max_length=15)
+
+    gender = models.CharField(max_length=10)
+
+    address = models.TextField()
 
     def __str__(self):
         return self.full_name
+class LuggageBooking(models.Model):
+    passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+
+    source_station = models.CharField(max_length=100)
+    destination_station = models.CharField(max_length=100)
+
+    luggage_type = models.CharField(max_length=100)
+    number_of_bags = models.IntegerField()
+    weight = models.FloatField()
+
+    travel_date = models.DateField()
+    contact_number = models.CharField(max_length=15)
+
+    status = models.CharField(max_length=20, default="Pending")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.passenger.full_name
--- home/views.py
+++ home/views.py
@@ -16,9 +16,9 @@ def login(request):
                 username=username,
                 password=password
             )
-            return render(request, "home/dashboard.html", {
-            "name": passenger.full_name
-            })
+            request.session["passenger_id"] = passenger.id
+            request.session["passenger_name"] = passenger.full_name
+            return redirect("dashboard")
 
         except Passenger.DoesNotExist:
             messages.error(request, "Invalid Username or Password")
@@ -32,13 +32,91 @@ def register(request):
         full_name = request.POST["full_name"]
         email = request.POST["email"]
         username = request.POST["username"]
+        aadhaar_number = request.POST["aadhaar_number"]
+        phone_number = request.POST["phone_number"]
+        gender = request.POST["gender"]
+        address = request.POST["address"]
         password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        # Check password
+        if password != confirm_password:
+             return render(request, "home/register.html", {
+        "error": "Passwords do not match!",
+        "full_name": full_name,
+        "email": email,
+        "username": username,
+        "aadhaar_number": aadhaar_number,
+        "phone_number": phone_number,
+        "gender": gender,
+        "address": address,
+    })
+
+        # Check username
+        if Passenger.objects.filter(username=username).exists():
+            return render(request, "home/register.html", {
+                "error": "Username already exists!"
+            })
+
+        # Check email
+        if Passenger.objects.filter(email=email).exists():
+            return render(request, "home/register.html", {
+                "error": "Email already exists!",
+                 "full_name": full_name,
+                "email": email,
+                 "username": username,
+                 "aadhaar_number": aadhaar_number,
+                "phone_number": phone_number,
+                 "gender": gender,
+                "address": address,
+    })
+
+        # Check Aadhaar
+        if Passenger.objects.filter(aadhaar_number=aadhaar_number).exists():
+            return render(request, "home/register.html", {
+            "error": "Some error",
+            "full_name": full_name,
+            "email": email,
+            "username": username,
+             "aadhaar_number": aadhaar_number,
+             "phone_number": phone_number,
+             "gender": gender,
+            "address": address,
+        })
+
+        if not aadhaar_number.isdigit() or len(aadhaar_number) != 12:
+          return render(request, "home/register.html", {
+        "error": "Some error",
+        "full_name": full_name,
+        "email": email,
+        "username": username,
+        "aadhaar_number": aadhaar_number,
+        "phone_number": phone_number,
+        "gender": gender,
+        "address": address,
+        })
+
+        if not phone_number.isdigit() or len(phone_number) != 10:
+            return render(request, "home/register.html", {
+            "error": "Some error",
+            "full_name": full_name,
+            "email": email,
+            "username": username,
+            "aadhaar_number": aadhaar_number,
+            "phone_number": phone_number,
+            "gender": gender,
+            "address": address,
+        })
 
         Passenger.objects.create(
             full_name=full_name,
             email=email,
             username=username,
-            password=password
+            password=password,
+            aadhaar_number=aadhaar_number,
+            phone_number=phone_number,
+            gender=gender,
+            address=address,
         )
 
         return render(request, "home/register.html", {
@@ -46,3 +124,13 @@ def register(request):
         })
 
     return render(request, "home/register.html")
+def logout(request):
+    request.session.flush()
+    return redirect("home")
+def dashboard(request):
+    if "passenger_id" not in request.session:
+        return redirect("login")
+
+    return render(request, "home/dashboard.html", {
+        "name": request.session["passenger_name"]
+    })
